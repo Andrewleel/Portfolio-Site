a873e6e7ae21f568fc04968a8fb825e585afc25d
Andrew Lee <andrewslee030401@gmail.com>
2023-08-13 02:14:47 -0700

    added simple styles

---
+++ src/components/Project.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Project = ( props ) => {
+  return (
+    <div>
+      <h2>{ props.name }</h2>
+      <h3>{ props.tools }</h3>
+      <ul>
+        {
+          props.description.map((each) => {
+            return <li>{ each }</li>
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default Project
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,11 +3,15 @@ import Profile from './../components/Profile'
 import Description from './../components/Description'
 import SocialMedia from './../components/SocialMedia'
 import Header from './../components//Header'
+import Project from './../components/Project'
+import './Main.css'
 
 const Main = () => {
   return (
     <div>
-        <Header />
+        <div className ="header">
+            <Header />
+        </div>
         <Profile />
         <Description />
         <SocialMedia />
